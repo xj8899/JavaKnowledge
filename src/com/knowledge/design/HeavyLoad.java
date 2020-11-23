5632ba02c97938b72dae0efb3d7febefe80bbdd4
xujie <xj88992@163.com>
2020-11-23 21:41:37 +0800

    添加里氏替换原则

---
+++ src/com/knowledge/design/HeavyLoad.java
@@ -0,0 +1,78 @@
+package com.knowledge.design;
+
+
+/**
+ * 里氏替换原则
+ */
+public class HeavyLoad {
+
+    public static void main(String[] args) {
+        Son son = new Son("xujie");
+        son.sayHello(21);
+        son.sayHello("Hello");
+    }
+
+
+}
+
+
+
+class Parent{
+
+    Parent(String name){
+        this.name = name;
+    }
+
+    Parent(Integer age){
+        this.age = age;
+    }
+
+    Parent(String name , Integer age){
+        this.name = name ;
+        this.age  = age ;
+    }
+    private String name;
+
+    private Integer age;
+
+    public void sayHello(String msg){
+        System.out.println(name+":"+msg);
+    }
+
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class Son extends Parent{
+
+    Son(String name) {
+        super(name);
+    }
+
+    Son(Integer age) {
+        super(age);
+    }
+
+    Son(String name, Integer age) {
+        super(name, age);
+    }
+
+    public void sayHello(int age){
+        System.out.println(this.getName()+":"+age);
+    }
+
+}
---
+++ src/com/knowledge/design/Isp.java
@@ -0,0 +1,45 @@
+package com.knowledge.design;
+
+
+public class Isp {
+
+
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.Count();
+        teacher.Input();
+        teacher.Print();
+    }
+
+}
+
+
+interface InputModule{
+    void Input();
+}
+
+interface CountModule{
+    void Count();
+}
+
+interface PrintModule{
+    void Print();
+}
+
+class Teacher implements InputModule,CountModule,PrintModule{
+
+    @Override
+    public void Input() {
+        System.out.println("Input");
+    }
+
+    @Override
+    public void Count() {
+        System.out.println("Count");
+    }
+
+    @Override
+    public void Print() {
+        System.out.println("Print");
+    }
+}
